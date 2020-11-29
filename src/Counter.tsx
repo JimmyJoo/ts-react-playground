c22a8e30b42a942ea12c59dfb0b7c499312bb834
JimmyJoo <jimmyjoo1029@gmail.com>
2020-11-29 10:42:15 +0900

    :art: Change useState to useReducer in Counter component

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,10 +1,28 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+type Action = { type: 'INCREASE' } | { type: 'DECREASE' };
+
+function counterReducer(state: number, action: Action): number {
+  switch (action.type) {
+    case 'INCREASE':
+      return state + 1;
+    case 'DECREASE':
+      return state - 1;
+    default:
+      throw new Error('Unhandled action type');
+  }
+}
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
+  // const onIncrease = () => setCount(count + 1);
+  // const onDecrease = () => setCount(count - 1);
+
+  const [count, dispatch] = useReducer(counterReducer, 0);
+
+  const onIncrease = () => dispatch({ type: 'INCREASE' });
+  const onDecrease = () => dispatch({ type: 'DECREASE' });
 
   return (
     <div>
